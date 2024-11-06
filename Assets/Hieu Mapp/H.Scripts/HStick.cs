e1f18d651602fa3de2b39aa0bbb8f17b2484ae26
HieuCantCook <157356547+HieuCantCook@users.noreply.github.com>
2024-11-06 22:20:16 +0100

    Stick not using interact script

--- Assets/Hieu Mapp/H.Scripts/HStick.cs
+++ Assets/Hieu Mapp/H.Scripts/HStick.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class HStick : MonoBehaviour, HInteract
 {
-    
+    private CapsuleCollider c;
 
     private void Start()
     {
+        c = gameObject.GetComponent<CapsuleCollider>();
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            c.enabled = false;
+        }
     }
 
     public void Interact()
     {
         CapsuleCollider c = gameObject.GetComponent<CapsuleCollider>();
+        c.enabled = false;
         
     }
 
