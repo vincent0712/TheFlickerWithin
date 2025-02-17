c39d7a2b10390f19dffd2127ca6726a49096da0a
Jonathan-Mellberg <158470120+Jonathan-Mellberg@users.noreply.github.com>
2025-02-17 13:50:52 +0100

    Update menuScript.cs

--- Assets/Jonathan Mapp/Scripts.J/menuScript.cs
+++ Assets/Jonathan Mapp/Scripts.J/menuScript.cs
@@ -1,11 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEditor.UI;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 public class menuScript : MonoBehaviour
 {
+    public AudioMixer audio;
+    public Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+    }
+
     public void SetVolume(float volume)
     {
-        Debug.Log(volume);
+        audio.SetFloat("masterVolume", volume);
+    }
+
+    public void SetResolution(int resolutionIndew)
+    {
+        
+    }
+  
+    public void SetQuality(int Qualityindex)
+    {
+        QualitySettings.SetQualityLevel(Qualityindex);
     }
 }
