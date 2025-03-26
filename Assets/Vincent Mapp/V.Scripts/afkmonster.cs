dc434499bd225e49c53da8f3776070477725b0c7
vincent0712 <vincent.jonsson@elev.ga.lbs.se>
2025-03-26 13:25:49 +0100

    awd

---
+++ Assets/Vincent Mapp/V.Scripts/afkmonster.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class afkmonster : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animation anim;
+    void Start()
+    {
+        anim.Play("idle");
+    }
+
+
+}
