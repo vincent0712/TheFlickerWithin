ac09c55fc78537c7cb37b2e302b9266320ea9c3e
vincent0712 <vincent.jonsson@elev.ga.lbs.se>
2025-03-26 12:40:16 +0100

    new

--- Assets/Hieu Mapp/H.Scripts/HPlayerDead.cs
+++ Assets/Hieu Mapp/H.Scripts/HPlayerDead.cs
@@ -17,12 +17,12 @@ public class HPlayerDead : MonoBehaviour
     {
         if(other.gameObject.tag == "monster")
         {
-<<<<<<< HEAD
+            SceneManager.LoadScene("Outro");
             StartCoroutine(kill());
             
-=======
-            SceneManager.LoadScene("Outro");
->>>>>>> efce5190ce6268c00ee87a05b05ed308f020d6ab
+
+            
+
         }
     }
 
