1d0b4682c95d305eccb8ac8db6aa580fc4c0f667
vincent0712 <vincent.jonsson@elev.ga.lbs.se>
2025-03-31 11:43:59 +0200

    wew

--- Assets/Oliver Mapp/Minispel labyrint gnista/Timeer.cs
+++ Assets/Oliver Mapp/Minispel labyrint gnista/Timeer.cs
@@ -1,50 +1,62 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Timeer : MonoBehaviour
 {
     public int duration = 25;
     public int timeRemaining;
-    public bool isCountingDown = false;
+    private bool isCountingDown = false;
     public TextMeshProUGUI timerTxT;
     public bool gameOver = false;
     private Fusebox fuse;
     public bool win = false;
     public Puzzlehandeler puzzlehandeler;
     private AudioSource fuseaudio;
+    private Coroutine countdownCoroutine;
 
     private void Start()
     {
         fuse = GameObject.FindGameObjectWithTag("fuse").GetComponent<Fusebox>();
-        timerTxT.text = duration.ToString();
         fuseaudio = GameObject.FindGameObjectWithTag("fuse").GetComponent<AudioSource>();
+        timerTxT.text = duration.ToString();
     }
-    private void Update()
+
+    private void OnEnable()
     {
+        // Reset the timer when the minigame is enabled
+        timeRemaining = duration;
+        timerTxT.text = timeRemaining.ToString();
+        isCountingDown = true;
 
-        if (!isCountingDown)
-
+        // Start the timer
+        if (countdownCoroutine == null)
         {
-            isCountingDown = true;
-            timeRemaining = duration;
-            Invoke("_tick", 1f);
+            countdownCoroutine = StartCoroutine(Countdown());
         }
     }
 
-    private void _tick()
+    private void OnDisable()
     {
-        timeRemaining--;
-        //Debug.Log(timeRemaining);
-        if (timeRemaining >= 0)
+        // Stop the timer when the minigame is disabled
+        isCountingDown = false;
+        if (countdownCoroutine != null)
         {
+            StopCoroutine(countdownCoroutine);
+            countdownCoroutine = null;
+        }
+    }
+
+    private IEnumerator Countdown()
+    {
+        while (timeRemaining > 0 && isCountingDown)
+        {
+            yield return new WaitForSeconds(1f);
+            timeRemaining--;
             timerTxT.text = timeRemaining.ToString();
-            Invoke("_tick", 1f);
         }
-        else
+
+        if (isCountingDown) // Ensure game logic triggers only if the game wasn't turned off
         {
             isCountingDown = false;
             gameOver = true;
@@ -53,5 +65,8 @@ public class Timeer : MonoBehaviour
             fuseaudio.Play();
             puzzlehandeler.Turnoffgame();
         }
+
+        // Reset coroutine reference when done
+        countdownCoroutine = null;
     }
 }
