1f6836432948b82753465820bcba0a851fa0625e
vincent0712 <vincent.jonsson@elev.ga.lbs.se>
2025-04-07 10:32:42 +0200

    new

--- Assets/Vincent Mapp/V.Scripts/tutorial.cs
+++ Assets/Vincent Mapp/V.Scripts/tutorial.cs
@@ -18,7 +18,7 @@ public class tutorial : MonoBehaviour
 
         }
 
-        if (Input.GetKeyDown(KeyCode.T) && !anim.isPlaying)
+        if (Input.GetKeyDown(KeyCode.T) && !anim.isPlaying && !isshowing)
         {
             fadein();
         }
